79f94bc3b007bd9ad1f935776a467e07eccaa013
yereumi <lovelyyeji01@gmail.com>
2026-07-30 11:53:41 +0900

    feat: num에 해당하는 TripDto 정보 검색 기능 구현

--- EnjoyTrip/src/com/ssafy/trip/model/dao/TripDaoImpl.java
+++ EnjoyTrip/src/com/ssafy/trip/model/dao/TripDaoImpl.java
@@ -54,7 +54,12 @@ public class TripDaoImpl implements TripDao {
 		// complete code #03
 		// List<TripDto> tripInfo 로부터 num 에 해당하는 TripDto 정보를 검색하여 return 하도록 코드를 작성하세요.
 		// 해당하는 num이 없을 경우 null을 리턴하세요.
-
+		for (TripDto tripDto : tripInfo) {
+			if (tripDto.getNum() == num) {
+				return tripDto;
+			}
+		}
+		
 		return null;
 	}
 

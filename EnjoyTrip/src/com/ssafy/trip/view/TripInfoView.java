f0884824c6a98ca8b8d3e6c4f3483984cb526a3b
yereumi <lovelyyeji01@gmail.com>
2026-07-30 11:52:51 +0900

    feat: TripInfoView 람다로 구현

--- EnjoyTrip/src/com/ssafy/trip/view/TripInfoView.java
+++ EnjoyTrip/src/com/ssafy/trip/view/TripInfoView.java
@@ -188,16 +188,18 @@ public class TripInfoView {
 		// complete code #01
 		// 아래의 코드를 참조하여 아래 라인을 uncomment 하고 searchBt.addActionList() 를 Lambda 표현식으로 바꾸세요.
 		// searchBt.addActionListener( /* 여기 */ );
+		
+		searchBt.addActionListener(e -> searchTrips());
 
 		// 참조코드 시작 - 위 코드를 완성 후 삭제 또는 comment 처리하세요.
-		ActionListener buttonHandler = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				searchTrips();
-			}
-		};
-		
-		searchBt.addActionListener( buttonHandler );
+//		ActionListener buttonHandler = new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				searchTrips();
+//			}
+//		};
+//		
+//		searchBt.addActionListener( buttonHandler );
 		// 참조코드 종료
 
 		showTrips();
